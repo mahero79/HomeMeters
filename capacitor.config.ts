46d0d0f10fa4e09e1359ca5203b931571c13840d
Maher Alhalabi <mahero79@gmail.com>
2026-03-21 18:55:42 +0100

    Android version - Capacitor app

---
+++ capacitor.config.ts
@@ -0,0 +1,7 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.homemeters.app',
+  appName: 'Home Meters',
+  webDir: 'www'
+};
+export default config;
